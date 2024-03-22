c88f342827375be1825ce7f59dd3396672c9bfb9
Phat <nvtpk19hutech@gmail.com>
2024-03-22 16:24:00 +0700

    213

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,6 @@ export default function Home() {
   return (
     <>
    <div className="bg-gray-900 h-screen flex flex-col items-center justify-center text-white">
-  <h1 className="text-4xl font-bold mb-6">Coming Soon</h1>
-
-  <div className="flex text-2xl mb-6">
-    <div className="mr-2">
-      <span id="days" className="font-semibold">00</span> Days
-    </div>
-    <div className="mr-2">
-      <span id="hours" className="font-semibold">00</span> Hours
-    </div>
-    <div className="mr-2">
-      <span id="minutes" className="font-semibold">00</span> Minutes
-    </div>
-    <div>
-      <span id="seconds" className="font-semibold">00</span> Seconds
-    </div>
-  </div>
-
-  <form className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
-    <input
-      type="email"
-      placeholder="Enter your email"
-      className="rounded-full px-4 py-2 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-400"
-    />
-    <button
-      type="submit"
-      className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-full transition duration-300"
-    >
-      Subscribe
-    </button>
-  </form>
-
   <div className="mt-4 space-x-4">
     <a href="#" className="text-gray-400 hover:text-white">
       <i className="fab fa-facebook-f"></i>
@@ -48,8 +17,6 @@ export default function Home() {
   </div>
   <section id="testimonies" className="py-20 bg-slate-900">
     <div className="max-w-6xl mx-8 md:mx-10 lg:mx-20 xl:mx-auto">
-
-
         <div className="transition duration-500 ease-in-out transform scale-100 translate-x-0 translate-y-0 opacity-100">
             <div className="mb-12 space-y-5 md:mb-16 md:text-center">
                 <div
