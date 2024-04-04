c9dbdf4f34c06b4e31388b33d766d93dfe8bd7d2
Phat <nvtpk19hutech@gmail.com>
2024-04-04 10:25:54 +0700

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,26 @@
-import Image from "next/image";
 
 export default function Home() {
     return (
         <>
-            <div>
-                <h1>Tấn Phát Store</h1>
-            </div>
             <div className="p-5 sm:p-8">
                 <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-                    <img src="https://source.unsplash.com/bYuI23mnmDQ" />
-                    <img src="https://source.unsplash.com/Nllx4R-2c3o" />
-                    <img src="https://source.unsplash.com/lp40q07DIe0" />
-                    <img src="https://source.unsplash.com/wfalq01jJuU" />
-                    <img src="https://source.unsplash.com/rMHNK_skwwU" />
-                    <img src="https://source.unsplash.com/WBMjuGpbrCQ" />
-                    <img src="https://source.unsplash.com/nCUZ5BYBL_o" />
-                    <img src="https://source.unsplash.com/3u4fzMQZhjc" />
-                    <img src="https://source.unsplash.com/haOIqIPSwps" />
-                    <img src="https://source.unsplash.com/3UrYD7NNVxk" />
-                    <img src="https://source.unsplash.com/fm1JKDItlVM" />
-                    <img src="https://source.unsplash.com/qPpq1EVs8vw" />
-                    <img src="https://source.unsplash.com/xRyL63AwZFE" />
-                    <img src="https://source.unsplash.com/XeNKWTiCPNw" />
-                    <img src="https://source.unsplash.com/DFt3T5r_4FE" />
-                    <img src="https://source.unsplash.com/Ebwp2-6BG8E" />
-                    <img src="https://source.unsplash.com/bYuI23mnmDQ" />
+                    <img src="https://source.unsplash.com/bYuI23mnmDQ" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/Nllx4R-2c3o" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/lp40q07DIe0" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/wfalq01jJuU" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/rMHNK_skwwU" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/WBMjuGpbrCQ" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/nCUZ5BYBL_o" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/3u4fzMQZhjc" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/haOIqIPSwps" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/3UrYD7NNVxk" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/fm1JKDItlVM" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/qPpq1EVs8vw" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/xRyL63AwZFE" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/XeNKWTiCPNw" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/DFt3T5r_4FE" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/Ebwp2-6BG8E" className="h-1/5"/>
+                    <img src="https://source.unsplash.com/bYuI23mnmDQ" className="h-1/5"/>
                 </div>
             </div>
         </>
